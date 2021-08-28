d3aca99d6e83c57e108879ed52d3ec82607a78cb
Atria <49768768+Atria64@users.noreply.github.com>
2021-08-29 02:22:45 +0900

    EditTextの中身が0のときボタンの色を変更できるようにWIP

--- front/app/src/main/java/spajam/yowayowa/prespajam2021/UsernameSettingFragment.kt
+++ front/app/src/main/java/spajam/yowayowa/prespajam2021/UsernameSettingFragment.kt
@@ -2,11 +2,16 @@ package spajam.yowayowa.prespajam2021
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
+import org.w3c.dom.Text
+
 
 class UsernameSettingFragment : Fragment() {
     override fun onCreateView(
@@ -18,7 +23,6 @@ class UsernameSettingFragment : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         val button = view.findViewById<Button>(R.id.button)
         button.setOnClickListener(){
             val intent = Intent(activity, MainActivity::class.java)
@@ -26,5 +30,24 @@ class UsernameSettingFragment : Fragment() {
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
             startActivity(intent)
         }
+        val editText = view.findViewById<EditText>(R.id.editTextTextPersonName)
+        editText.addTextChangedListener(object : TextWatcher {
+            // 文字列sのなかで startの位置から開始されている字数countの文字が、
+            // 字数lengthの古いテキストを置換するときに呼び出される。
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                // TODO Auto-generated method stub
+            }
+
+            // 文字列sのなかで startの位置から開始されている字数countの文字が、
+            // 字数lengthの新しいテキストで置換されようとしているときに呼び出される。
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                // TODO Auto-generated method stub
+            }
+
+            // 文字列sのどこかで、テキストが変更されたときに呼び出される。
+            override fun afterTextChanged(s: Editable) {
+                button.isEnabled = !(s.length === 0)
+            }
+        })
     }
 }
