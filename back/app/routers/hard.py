8b3cbd862a484ad07a2085cb22837c3518fbf6ea
t4t5u0 <ymmtryk0902@gmail.com>
2021-08-29 02:11:55 +0900

    Add: hardのモックを追加

--- back/app/routers/hard.py
+++ back/app/routers/hard.py
@@ -3,10 +3,11 @@ from pydantic import BaseModel
 
 router = APIRouter()
 
+
 class MacAddress(BaseModel):
     mac_address: str
 
 
 @router.post("/hard/sendstring")
-async def send_string(self):
+async def send_string(mac_address: MacAddress):
     return {"string": "1nyarn"}
